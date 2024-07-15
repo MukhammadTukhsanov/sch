e5f13cd132726c63a730d3d965d5db9a6a1e0a57
Mukhammad <muxammadhotamo@gmail.com>
2024-07-15 13:57:38 +0400

    Initial commit

---
+++ src/components/BaseFileUpload/BaseFileUpload.js
@@ -0,0 +1,11 @@
+import Form from 'react-bootstrap/Form';
+
+const BaseFileUpload = ({ file, value }) => {
+    return (
+        <Form.Group controlId="formFileLg" className="mb-3">
+            <Form.Control value={value} onChange={(e) => file(e)} type="file" size="lg" multiple placeholder='Bild upload' />
+        </Form.Group>
+    )
+}
+
+export default BaseFileUpload
